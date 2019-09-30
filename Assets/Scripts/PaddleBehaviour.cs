00ebfc2d2c4a25bc526d2f37338d24a6927c8284
João Vinagre <jpvinagre6@gmail.com>
2019-10-01 00:46:26 +0100

    Aula 24/09

---
+++ Assets/Scripts/PaddleBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehaviour : MonoBehaviour
+{
+    public void MovePaddle(float _dirVal)
+    {
+        Vector3 newPos = transform.position + Vector3.up * 5f * Time.deltaTime * _dirVal;
+        newPos.y = Mathf.Clamp(newPos.y, -3.9f, 3.9f); 
+        transform.position = newPos;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PaddleBehaviour paddleBeh;
+    private string playerAxis;
+
+    void Awake()
+    {
+        paddleBeh = GetComponent<PaddleBehaviour>();
+        if (gameObject.name.Contains("P1"))
+        {
+            playerAxis = "Vertical_P1";
+        }
+        else if (gameObject.name.Contains("P2"))
+        {
+            playerAxis = "Vertical_P2";
+        }
+    }
+
+    void Update()
+    {
+        float moveDir = Input.GetAxis(playerAxis);
+        paddleBeh.MovePaddle(moveDir);
+
+    }
+}
